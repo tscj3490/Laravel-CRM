f0b12e477ddbdc1a35ea4f84b179bd80db1a546a
Bluebarricade <44467209+BlueBarricade@users.noreply.github.com>
2022-10-28 11:03:01 +0800

    added a router for show user

--- routes/web.php
+++ routes/web.php
@@ -25,15 +25,6 @@ Route::group(['middleware'=>'auth'],function () {
     Route::get('/logs/delete/{logSlug}', 'LogController@delete')->name('deleteLog');
     Route::delete('/logs/delete/{logSlug}', 'LogController@destroy')->name('destroyLog');
 
-    // Client routes
-    Route::get('/clients', 'ClientController@index')->name('clientsHome');
-    Route::get('/clients/create', 'ClientController@create')->name('createClient');
-    Route::post('/clients/create', 'ClientController@store')->name('createClient');
-    Route::get('/clients/{clientSlug}', 'ClientController@show')->name('showClient');
-    Route::get('/clients/edit/{logSlug}', 'ClientController@edit')->name('editClient');
-    Route::patch('/clients/update/{logSlug}', 'ClientController@update')->name('updateClient');
-    Route::delete('/clients/delete/{logSlug}', 'ClientController@destroy')->name('destroyClient');
-
     // User routes
     Route::get('/users', 'UserController@index')->name('usersHome');
     Route::get('/users/create', 'UserController@create')->name('createUser');
@@ -43,6 +34,15 @@ Route::group(['middleware'=>'auth'],function () {
     Route::patch('/users/update/{user}', 'UserController@update')->name('updateUser');
     Route::get('/users/delete/{user}', 'UserController@delete')->name('deleteUser');
     Route::delete('/users/delete/{user}', 'UserController@destroy')->name('destroyUser');
+    
+    // Client routes
+    Route::get('/clients', 'ClientController@index')->name('clientsHome');
+    Route::get('/clients/create', 'ClientController@create')->name('createClient');
+    Route::post('/clients/create', 'ClientController@store')->name('createClient');
+    Route::get('/clients/{clientSlug}', 'ClientController@show')->name('showClient');
+    Route::get('/clients/edit/{logSlug}', 'ClientController@edit')->name('editClient');
+    Route::patch('/clients/update/{logSlug}', 'ClientController@update')->name('updateClient');
+    Route::delete('/clients/delete/{logSlug}', 'ClientController@destroy')->name('destroyClient');
 
     // User settings routes
     Route::get('/settings', 'Auth\UserSettingsController@edit')->name('editSettings');
